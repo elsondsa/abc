404f7ef917f1fd95ba252b6270f261a27f06d4c3
unknown <comviva@MCKD-TRNG60-42.COMVIVA.COM>
2020-02-18 09:14:16 +0530

    first

---
+++ src/main/java/com/viv/els/Course.java
@@ -0,0 +1,47 @@
+package com.viv.els;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name="findAllCourses", query="from Course e")
+public class Course {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int id;
+	
+	
+	String title;	
+	String summary;
+	public Course(){}
+	public Course(String title, String summary) {
+
+		this.title = title;
+		this.summary = summary;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + ", summary=" + summary + "]";
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/viv/els/CourseRepo.java
@@ -0,0 +1,9 @@
+package com.viv.els;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepo extends JpaRepository<Course, Integer> {
+
+}
